b5ea8ae6a0cce00b0b989aeabba5120d8304c0a6
FisKusT <sorte411@gmail.com>
2026-02-16 14:26:01 +0200

    SLURM RUNS

---
+++ slurm_run/l40_lab_run27.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=suft_L27
+#SBATCH --partition=shaham-L40
+#SBATCH --gres=gpu:1
+#SBATCH --mem=100G
+#SBATCH --mail-user=fiskust@biu.ac.il
+#SBATCH --mail-type=END
+
+
+cd /home/dsi/fiskustal/talfiskus/dopamine && source venv/bin/activate && CUDA_VISIBLE_DEVICES=0 nohup python -u -m bbf.train --agent=BBF --gin_files=bbf/configs/BBF_search_config4.gin --base_dir=suft2/suft_optimization_1000_same_reset_tf_1_0_huber_run7_Kangaroo_v5 --agent_seed=63049806   --no_seeding=False --run_number=77 --game_name="Kangaroo" > "suft2/suft_optimization_1000_same_reset_tf_1_0_huber_run7_Kangaroo_v5.log" 2>&1 &
+
+wait
---
+++ slurm_run/l40_lab_run4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=suft_L4
+#SBATCH --partition=shaham-L40
+#SBATCH --gres=gpu:1
+#SBATCH --mem=100G
+#SBATCH --mail-user=fiskust@biu.ac.il
+#SBATCH --mail-type=END
+
+
+cd /home/dsi/fiskustal/talfiskus/dopamine && source venv/bin/activate && CUDA_VISIBLE_DEVICES=0 nohup python -u -m bbf.train --agent=BBF --gin_files=bbf/configs/BBF_search_config1.gin --base_dir=suft2/suft_optimization_500_tf_1_0_huber_run4_Kangaroo_v5 --agent_seed=950649544  --no_seeding=False --run_number=74 --game_name="Kangaroo" > "suft2/suft_optimization_500_tf_1_0_huber_run4_Kangaroo_v5.log" 2>&1 &
+
+wait
